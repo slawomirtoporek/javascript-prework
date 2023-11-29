562f7fe7c8c48ade9c9cdc375c1a60a7c2a6d5ad
Sławomir Toporek <toporek.slawomir@gmail.com>
2023-11-29 12:25:23 +0100

    squash! add buttons and function playGame

--- js/script.js
+++ js/script.js
@@ -21,9 +21,7 @@ function playGame(playerInput){
 
     printMessage('Mój ruch to: ' + computerMove);
 
-
-    //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
+    
     console.log('Gracz wpisał: ' + playerInput);
 
     let playerMove = getMoveName(playerInput);
